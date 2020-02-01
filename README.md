638c596c094255903eb481fcb7b253065709b7a8
Mattias Päivärinta <mattias@paivarinta.se>
2020-02-01 16:27:11 +0100

    New distribution

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Signal::Queue - It's new $module
+
+# SYNOPSIS
+
+    use Signal::Queue;
+
+# DESCRIPTION
+
+Signal::Queue is ...
+
+# LICENSE
+
+Copyright (C) Mattias Päivärinta.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Mattias Päivärinta <mattias@paivarinta.se>
---
+++ lib/Signal/Queue.pm
@@ -0,0 +1,39 @@
+package Signal::Queue;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Signal::Queue - It's new $module
+
+=head1 SYNOPSIS
+
+    use Signal::Queue;
+
+=head1 DESCRIPTION
+
+Signal::Queue is ...
+
+=head1 LICENSE
+
+Copyright (C) Mattias Päivärinta.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Mattias Päivärinta E<lt>mattias@paivarinta.seE<gt>
+
+=cut
+
